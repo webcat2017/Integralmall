b12c86401e2babca21575748a27e211a27591b9e
webcat <1730684389@qq.com>
2017-09-08 14:37:25 +0800

    2017-9-8-1

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    //'配置项'=>'配置值'
+    'db_type'  => 'mysql',
+    'db_user'  => 'root',
+    'db_pwd'   => 'root',
+    'db_host'  => 'localhost',
+    'db_port'  => '3306',
+    'db_name'  => 'scoreshop',
+    'DB_PREFIX'  => 'sc_',
+    'db_charset'=>  'utf8',
+);
